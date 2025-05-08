3ce00c72d35880433b5e4cb9e6573f88316ae7ea
Govind Puniani <gpuniani7@gmail.com>
2025-05-08 14:40:25 +0530

    Fix: Add base path for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,32 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/monaco-editor/min/vs',
+          dest: 'vs'
+        }
+      ]
+    })
+  ],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    target: 'esnext',
+    minify: 'esbuild',
+    sourcemap: false,
+    assetsInlineLimit: 4096
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      }
+    }
+  }
 });
